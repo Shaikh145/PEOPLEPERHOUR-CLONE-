d0607aed65a070e1cbcd331d03a85fe66276e228
Abd Ur Rahman Shaikh EarningWala <arshaikhwebdeveloperwala@gmail.com>
2025-05-04 18:20:52 +0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'db.php';
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to home page
+jsRedirect('index.php');
+?>
